ba65932dcc63e7a7f5f57a92a1a5108e0a1213dd
JUA <mafls122@naver.com>
2021-04-12 01:48:50 +0900

    [ADD] Django

--- presentation_main/models.py
+++ presentation_main/models.py
@@ -1,2 +1,8 @@
 from django.db import models
 
+class ImageUpload(models.Model):
+    title = models.CharField(max_length=100)
+    pic = models.FileField(null=True, blank=True, upload_to="")
+
+    def __str__(self):
+        return self.title
--- presentation_main/views.py
+++ presentation_main/views.py
@@ -1,5 +1,21 @@
 from django.http import HttpResponse
-
+from .forms import UploadForm
+from django.shortcuts import render, redirect
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the presentation index.")
+    return render(request, 'pages/index.html', {})
+
+def image_list(request):
+    return render(request, 'pages/list.html', {})
+
+def upload_image(request):
+    if request.method == 'POST':
+        form = UploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('image_list')
+    else:
+        form = UploadForm()
+    return render(request, 'pages/upload.html', {'form':form})
+
+
